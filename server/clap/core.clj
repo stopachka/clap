4a9fea28b944a5271c8801b86380263b0e05b83a
stopachka <stepan.p@gmail.com>
2020-06-22 13:38:36 -0700

    set up 'build'

--- server/clap/core.clj
+++ server/clap/core.clj
@@ -1,17 +1,20 @@
 (ns clap.core
   (:require [ring.adapter.jetty :as jetty]
             [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
+            [compojure.route :refer [resources]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.params :refer [wrap-params]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response resource-response]]))
 
 (defn ping-handler [req]
   (response {:message "🏓 pong!"}))
 
 (defroutes
   routes
-  (GET "/api/ping" [] ping-handler))
+  (GET "/api/ping" [] ping-handler)
+  (resources "/" {:root "build"})
+  (GET "*" [] (fn [_] (response {:message "404"}))))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))
